ae668af0fd459c6fbfe1e312ff2a31a87f2be7e2
Shai Koffman <shaiko@gmail.com>
2024-03-14 21:24:46 +0200

    Fixes to make visa process cleanly

--- expenses.py
+++ expenses.py
@@ -2,20 +2,27 @@ import datetime
 
 from currency import CoinType
 from categories import Categories
-
-
-
+from builtins import NotImplementedError
 class Expense(object):
     def __init__(self):
         pass
 
-
+    def get_category(self) -> Categories:
+        raise NotImplementedError("This method should be implemented in a subclass")
+    
+    def get_transaction_date(self) -> datetime.datetime:
+        raise NotImplementedError("This method should be implemented in a subclass")
+    
+    def get_expense_sum(self) -> int:
+        raise NotImplementedError("This method should be implemented in a subclass")
+        
 class BankExpense(Expense):
     def __init__(self, date: datetime.datetime, company: str, expense: float):
         self.date = date
         self.company = company
         self.expense = expense
         self.category = self.get_category(company)
+    
     def __str__(self):
         return ("Bank Expense:\n"
                 "  date = {0}\n"
@@ -24,12 +31,17 @@ class BankExpense(Expense):
                 "  expense = {3}\n"
                 "  category = {4}\n"
                 .format(self.date, self.date.month, self.company, self.expense, self.category)) 
+    
+    def get_category(self, company: str) -> Categories:
+        return self.category
+    def get_transaction_date(self) -> datetime.datetime:
+        return self.date
+    def get_expense_sum(self) -> int:
+        return self.expense
 
 
 
 class VisaExpense(Expense):
-    
-    
     
     def __init__(self, sheet_title: str, company: str, known_category: Categories, visa_max_category: str, visa_card_number: str, expense_sum: int, coin: CoinType, expense_date: datetime.datetime):
         self.sheet_title = sheet_title
@@ -55,5 +67,13 @@ class VisaExpense(Expense):
                 "  normalized_expense_sum = {7}\n"
                 .format(self.sheet_title, self.company, self.visa_max_category, self.visa_card_number, self.expense_sum, self.coin.name, self.expense_date, self.normalized_expense_sum))
     
+    def get_category(self) -> Categories:
+        return self.known_category
+    
+    def get_transaction_date(self) -> datetime.datetime:
+        return self.expense_date
+    
+    def get_expense_sum(self) -> int:
+        return self.normalized_expense_sum
 
 
